d0935078a39100fc3e4581ddf1ec3c67bcc8684b
Egor Gorskikh <egorgorskikh@gmail.com>
2021-03-21 04:44:18 +0300

    first commit

---
+++ Chapter 1/HitList/HitList/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Егор Горских on 19.03.2021.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    @IBOutlet weak var tableView: UITableView!
+    var people: [NSManagedObject] = []
+    
+    // MARK: - Life Cicl
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "The List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+       fetch()
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else { return }
+            
+            self.save(name: nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    // MARK: - Core Data
+    
+    func save(name: String) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managetContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managetContext)!
+        let persone = NSManagedObject(entity: entity, insertInto: managetContext)
+        
+        persone.setValue(name, forKey: "name")
+    
+        do {
+            try managetContext.save()
+            people.append(persone)
+            print("Saved - \(persone)")
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func fetch() {
+        
+        guard  let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = people[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+        return cell
+        
+    }
+    
+    
+}
